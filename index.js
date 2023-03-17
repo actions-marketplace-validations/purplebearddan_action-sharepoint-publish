e69e5332ededb66e06bf177719b47f8553bc4db7
purplebearddan <daniel_m@purplebeard.co.uk>
2023-03-17 15:39:53 +0000

    added env printout

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ filePath = process.env.FILE_PATH
 fileExtensionArr = filePath.split('.')
 fileExtension = `.${fileExtensionArr[fileExtensionArr.length - 1]}`
 
+console.log(`[!!] Env File Path: ${filePath}`)
+
 const trimSlashes = (string) => {
     return string.replace(new RegExp('/', 'g'), '_')
 }
@@ -22,7 +24,7 @@ let coreOptions = {
 let fileOptions = {
     folder: process.env.LIB_FOLDER,
     fileName: `${trimSlashes(process.env.GITHUB_REPOSITORY)}${fileExtension}`,
-    fileContent: fs.readFileSync(process.env.FILE_PATH),
+    fileContent: fs.readFileSync(filePath),
 }
 
 spsave(coreOptions, creds, fileOptions)
